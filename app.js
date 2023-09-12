53626d0efac98b17b7f6153f1c3a373a581c992b
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2023-09-12 14:21:38 +0100

    Update app.js
    Minor changes

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
 app.use(morgan("combined"));
 
 require("dotenv").config();
